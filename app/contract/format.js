8b1c3a1915adb009fbb4df5eeb3459f5cad7737c
zkmgit <2821800369@qq.com>
2022-09-19 22:24:00 +0800

    用户模块编写接口文档

--- app/contract/format.js
+++ app/contract/format.js
@@ -2,6 +2,24 @@
 // 自动文档约定
 module.exports = {
   JsonBody: { // 这个名字对应上面 Controller 注释的@response 的 JsonBody。
-    result: { type: 'string' }, // 服务器返回的数据。
+    code: { type: 'string', required: true, example: '1' },
+    msg: { type: 'string', required: true, example: 'success' },
+    result: { type: 'object', required: true, example: {} },
   },
+  UserJsonBody: { // 这个名字对应上面 Controller 注释的@response 的 JsonBody。
+    code: { type: 'string', required: true, example: '1' },
+    msg: { type: 'string', required: true, example: 'success' },
+    result: { type: 'Array', required: true, example: [
+      {
+        username: 'string',
+        password: 'string',
+        name: 'string',
+        email: 'string',
+        status: 'integer',
+        sex: 'integer',
+        id: 'integer',
+      },
+    ] },
+  },
+
 };
--- app/controller/user.js
+++ app/controller/user.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-
+/**
+* @controller 用户模块
+*/
 class UserController extends Controller {
   async login() {
     const { ctx } = this;
@@ -28,6 +30,14 @@ class UserController extends Controller {
     };
     // ctx.body = user;
   }
+  /**
+    * @summary 用户列表分页查询
+    * @description 用户列表分页查询
+    * @router post /api/getAllUserList
+    * @request query integer pn 当前页数
+    * @request query string ps 每页x条
+    * @response 200 UserJsonBody 返回结果
+  */
   async getAllUserList() {
     const { ctx } = this;
     const params = ctx.request.body;
@@ -72,6 +82,18 @@ class UserController extends Controller {
       total,
     };
   }
+  /**
+    * @summary 用户新增
+    * @description 用户新增
+    * @router post /api/insertUser
+    * @request query string username 用户名
+    * @request query string password 密码
+    * @request query string name 姓名
+    * @request query string email 邮箱
+    * @request query integer sex 性别
+    * @request query integer status 状态
+    * @response 200 JsonBody 返回结果
+  */
   async insertUser() {
     const { ctx } = this;
     const params = ctx.request.body;
@@ -97,6 +119,19 @@ class UserController extends Controller {
       },
     };
   }
+  /**
+    * @summary 用户编辑
+    * @description 用户编辑
+    * @router put /api/updateUser
+    * @request query integer id id
+    * @request query string username 用户名
+    * @request query string password 密码
+    * @request query string name 姓名
+    * @request query string email 邮箱
+    * @request query integer sex 性别
+    * @request query integer status 状态
+    * @response 200 JsonBody 返回结果
+  */
   async updateUser() {
     const { ctx } = this;
     const params = ctx.request.body;
@@ -123,6 +158,13 @@ class UserController extends Controller {
     };
 
   }
+  /**
+    * @summary 根据id删除用户
+    * @description 根据id删除用户
+    * @router delete /api/deleteUser/:id
+    * @Request query string *id 用户id
+    * @response 200 JsonBody 返回结果
+  */
   async deleteUser() {
     const { ctx } = this;
     const params = ctx.params;
