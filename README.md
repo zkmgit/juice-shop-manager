1d9cde0643e9ead4ab795dbb52051daf27b42a29
zkmian3364 <zkmian@3364>
2022-11-04 15:02:07 +0800

    token白名单不拦截swagger资源

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ $ open http://localhost:7001/
 ```
 
 swagger文档开发地址
-http://localhost:7001//swagger-ui.html#/
+http://localhost:7001/swagger-ui.html#/
 
 ```配置
 数据库连接配置 ./config/config.default.js   搜索config.mysql
--- app/middleware/globalJwt.js
+++ app/middleware/globalJwt.js
@@ -1,4 +1,6 @@
-const whiteList = [ '/api/user/login' ];// 白名单（一般登录注册这两个接口不需要校验token）此处也可配置在全局
+// 白名单（一般登录注册这两个接口不需要校验token）此处也可配置在全局
+// swagger资源不拦截
+const whiteList = [ '/api/user/login', '/', '/swagger-ui.html', '/swagger-ui.css', '/swagger-ui-bundle.js', '/swagger-ui-standalone-preset.js', '/favicon-32x32.png', '/favicon-16x16.png', '/swagger-doc' ];
 
 module.exports = () => {
   return async function(ctx, next) {
