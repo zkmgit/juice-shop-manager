9826961bb0c68790a6e1e8d22678d2cdc2f235fe
Juice.Xu <juice.xu@duubee.com>
2023-03-16 13:41:39 +0800

    用户注册白名单

--- app/middleware/globalJwt.js
+++ app/middleware/globalJwt.js
@@ -1,6 +1,6 @@
 // 白名单（一般登录注册这两个接口不需要校验token）此处也可配置在全局
 // swagger资源不拦截
-const whiteList = [ '/wxApi/user/getToken', '/wxApi/user/login', '/wxApi/system/carouselImageList', '/wxApi/announcement/AnnouncementList', '/wxApi/category/getAllCategoryList',
+const whiteList = [ '/wxApi/user/getToken', '/wxApi/user/login', '/api/user/insertUser', '/wxApi/system/carouselImageList', '/wxApi/announcement/AnnouncementList', '/wxApi/category/getAllCategoryList',
                   '/wxApi/product/getAllProductList', '/wxApi/product/getAllProductListByCategoryId/:id', '/api/wxUser/login', '/api/user/login', '/',
                   '/wxApi/product/getProductInfoById/:id', '/wxApi/product/getAllSeckillProductList', '/wxApi/product/getAllRecommendedProductList',
                   '/swagger-ui.html', '/swagger-ui.css', '/swagger-ui-bundle.js', '/swagger-ui-standalone-preset.js',
